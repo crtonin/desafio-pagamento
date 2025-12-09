6d6318b13f1e5d5b097614f98a50a9c55fea56a6
crtonin <camila.rachel@gmail.com>
2025-12-09 18:07:47 -0300

    Import API_BASE_URL from constants

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080/api";
+import { API_BASE_URL } from "./constants";
 
 export async function enqueuePayment(payment) {
   const response = await fetch(`${API_BASE_URL}/payments/enqueue`, {
